698478968ee14866a088a97d30f36b150ffb2e23
faezahpatel <faezahpatel@gmial.com>
2024-01-12 13:33:13 +0530

    LoginScreen|trail

--- lib/Splash.dart
+++ lib/Splash.dart
@@ -1,19 +1,37 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:newparkingsolution/login.dart';
 
-class SplashScreen extends StatelessWidget{
+class SplashScreen extends StatefulWidget{
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 2),() {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen(),
+      ));
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.pink,
-        child: const Center(child: Text('Parking Solution',style: TextStyle(
-          fontSize: 34,
-          fontWeight: FontWeight.w700,
-          color: Colors.white,
-        ),)),
-      )
+        body: Container(
+          color: Colors.pink,
+          child: const Center(child: Text('Parking Solution',style: TextStyle(
+            fontSize: 34,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          ),)),
+        )
     );
   }
-
 }
